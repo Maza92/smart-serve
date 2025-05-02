7a17fa3296e31c52773e9fad5652b3dd8c8295c9
Maza92 <maza05599@gmail.com>
2025-05-02 16:51:05 -0500

    Add components page and theme preview styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ComponentsComponent } from './pages/components/components.page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'components',
+    component: ComponentsComponent,
+  },
+];
---
+++ src/app/pages/components/components.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-components',
+  standalone: true,
+  imports: [],
+  templateUrl: './components.page.html',
+  styleUrl: './components.page.css',
+})
+export class ComponentsComponent {}
