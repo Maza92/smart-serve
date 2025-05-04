269661f9fbffa3b0195a16239eb795b299d59efc
Maza92 <maza05599@gmail.com>
2025-05-04 16:09:49 -0500

    feat(home): update home component with new layout and sections

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink],
+  imports: [CommonModule, LucideAngularModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  sections: any = [
+    {
+      icon: 'square-check',
+      text: 'Inventario',
+      background: 'primary',
+      color: 'background',
+    },
+    {
+      icon: 'dollar-sign',
+      text: 'Caja',
+      background: 'background',
+      color: 'primary',
+    },
+    {
+      icon: 'chart-no-axes-column-increasing',
+      text: 'Reportes',
+      background: 'background',
+      color: 'primary',
+    },
+    {
+      icon: 'settings',
+      text: 'Ajustes',
+      background: 'primary',
+      color: 'background',
+    },
+  ];
+}
