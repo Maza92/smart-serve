45165652aa0fe70df94f34749c7879ab68911827
Maza92 <maza05599@gmail.com>
2025-05-04 20:48:12 -0500

    refactor(home): simplify dynamic class binding and add navigation exclusions

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { LucideAngularModule } from 'lucide-angular';
+import { NavigationService } from '../../core/service/navigation.service';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,22 @@ import { LucideAngularModule } from 'lucide-angular';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  private path: string | null = null;
+  constructor(private navigationService: NavigationService) {}
+
+  ngOnInit(): void {
+    this.path = this.navigationService.getCurrentComponentPath();
+
+    this.navigationService.addExclusion('Home', this.path);
+    this.navigationService.addExclusion('Inventario', this.path);
+    this.navigationService.addExclusion('Ajustes', this.path);
+    this.navigationService.addExclusion('Caja', this.path);
+    this.navigationService.addExclusion('Reportes', this.path);
+
+    this.navigationService.updateNavVisibility(this.path);
+  }
+
   sections: any = [
     {
       icon: 'square-check',
