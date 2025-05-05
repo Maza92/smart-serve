860947d43671d447945a4b63b3300cecc248cf0d
Maza92 <maza05599@gmail.com>
2025-05-04 20:48:00 -0500

    refactor(navigation-bar): simplify HTML and add dynamic navigation items

--- src/app/shared/navitagion-bar/navitagion-bar.component.ts
+++ src/app/shared/navitagion-bar/navitagion-bar.component.ts
@@ -1,11 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { LucideAngularModule } from 'lucide-angular';
+import { NavigationService } from '../../core/service/navigation.service';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { NavItem } from '../../core/model/navigation';
+import { filter, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-navigation-bar',
   standalone: true,
-  imports: [LucideAngularModule],
+  imports: [LucideAngularModule, RouterModule, CommonModule],
   templateUrl: './navigation-bar.component.html',
   styleUrl: './navigation-bar.component.css',
 })
-export class NavigationBarComponent {}
+export class NavigationBarComponent implements OnInit, OnDestroy {
+  navItems: NavItem[] = [];
+  private routerSubscription: Subscription | undefined;
+  private navSubscription: Subscription | undefined;
+
+  constructor(
+    private router: Router,
+    private navigationService: NavigationService
+  ) {}
+
+  ngOnInit(): void {
+    this.navSubscription = this.navigationService.visibleNavItems$.subscribe(
+      (items) => {
+        this.navItems = items;
+      }
+    );
+
+    this.routerSubscription = this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        this.navigationService.configureNavForRoute(event.url);
+      });
+
+    this.navigationService.configureNavForRoute(this.router.url);
+  }
+
+  ngOnDestroy(): void {
+    if (this.routerSubscription) {
+      this.routerSubscription.unsubscribe();
+    }
+    if (this.navSubscription) {
+      this.navSubscription.unsubscribe();
+    }
+  }
+}
